ccdb7116610bef91faddfcfd4b0d5084b02462a9
hosomi <hosomi@users.noreply.github.com>
2021-01-22 22:13:53 +0900

    Create #0007.reverse-integer.cs

---
+++ #0007.reverse-integer.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int Reverse(int x) {
+
+        long result = 0;
+		while (x != 0)
+        {
+			result = result * 10 + (x % 10);
+			x /= 10;
+		}
+ 
+		return Math.Abs(result) > Int32.MaxValue ? 0 : (int)result;
+    }
+}
