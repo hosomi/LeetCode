0b0ba856125330b964a8a94cabafd2bd8dc0c1fe
hosomi <hosomi@users.noreply.github.com>
2021-01-22 22:15:34 +0900

    Update #0007.reverse-integer.cs
    Indentation correction.

--- #0007.reverse-integer.cs
+++ #0007.reverse-integer.cs
@@ -2,12 +2,12 @@ public class Solution {
     public int Reverse(int x) {
 
         long result = 0;
-		while (x != 0)
+        while (x != 0)
         {
-			result = result * 10 + (x % 10);
-			x /= 10;
-		}
+            result = result * 10 + (x % 10);
+            x /= 10;
+        }
  
-		return Math.Abs(result) > Int32.MaxValue ? 0 : (int)result;
+        return Math.Abs(result) > Int32.MaxValue ? 0 : (int)result;
     }
 }
