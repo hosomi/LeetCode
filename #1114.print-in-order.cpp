bf239134c3700c1da199d7a0ad9eca16501c5864
hosomi <hosomi@users.noreply.github.com>
2021-01-23 19:57:57 +0900

    Create #1114.print-in-order.cpp

---
+++ #1114.print-in-order.cpp
@@ -0,0 +1,39 @@
+class Foo {
+
+    public:
+
+        void first(function<void()> printFirst) {
+            printFirst();
+            p1.set_value();
+        }
+
+        void second(function<void()> printSecond) {
+            p1.get_future().wait();
+            printSecond();
+            p2.set_value();
+        }
+
+        void third(function<void()> printThird) {
+            p2.get_future().wait();
+            printThird();
+        }
+
+
+    private:
+
+        std::promise<void> p1;
+        std::promise<void> p2;
+
+        void printFirst() {
+            cout << "first";
+        }
+
+        void printSecond() {
+            cout << "second";
+        }
+
+        void printThird() {
+            cout << "third";
+        }
+
+};
