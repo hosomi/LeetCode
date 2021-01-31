0ad38c5aba57213bd5ecbb0cdadae49f9021567c
hosomi <hosomi@users.noreply.github.com>
2021-01-31 19:00:04 +0900

    Create #0628.maximum-product-of-three-numbers.java

---
+++ #0628.maximum-product-of-three-numbers.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maximumProduct(int[] nums) {
+
+        int length = nums.length;
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[length - 1], nums[length - 1] * nums[length - 2] * nums[length - 3]);
+    }
+}
