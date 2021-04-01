e65f34291093a2c3b001121d9e4ac3188aad5dab
hosomi <hosomi@users.noreply.github.com>
2021-04-01 09:45:44 +0900

    Create #0066.plus-one.cpp

---
+++ #0066.plus-one.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+        int index = digits.size() -1;
+        while (index >= 0 && digits[index] == 9) {
+            digits[index] = 0;
+            index--;
+        }
+        
+        if (index < 0) {
+            digits.insert(digits.begin(), 1);
+        } else {
+            digits[index]++;
+        }
+
+        return digits;
+    }
+};
