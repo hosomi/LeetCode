a5e5d09f44f0bb41c03503270cae2b9e3a56219c
hosomi <hosomi@users.noreply.github.com>
2021-05-23 18:48:32 +0900

    Create #0122.best-time-to-buy-and-sell-stock-ii.cpp

---
+++ #0122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int size = prices.size();
+        int result = 0;
+        for (int i = 1; i < size; i++) {
+            if (prices[i] > prices[i-1]) {
+                result += prices[i] - prices[i-1];
+            }
+        }
+        return result;
+    }
+};
