58c4364f0bbab9cd18e0568548d436042d8213e5
hosomi <hosomi@users.noreply.github.com>
2021-07-29 09:37:18 +0900

    Create #0197.rising-temperature.sql

---
+++ #0197.rising-temperature.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT W2.id
+FROM
+  Weather AS W1
+  INNER JOIN Weather W2
+  ON W1.recordDate = DATEADD(day,-1, W2.recordDate)
+WHERE W2.temperature > W1.temperature
