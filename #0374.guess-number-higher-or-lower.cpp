71ae18790696781615ad2860a0bfa06d9911519c
hosomi <hosomi@users.noreply.github.com>
2021-11-25 09:21:57 +0900

    Create #0374.guess-number-higher-or-lower.cpp

---
+++ #0374.guess-number-higher-or-lower.cpp
@@ -0,0 +1,33 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+
+        int left = 1;
+        int right = n;
+        int cursor = left + (right - left) / 2;
+        while (true) {
+            switch (guess(cursor)){
+                case -1:
+                    right = cursor - 1;
+                    cursor = left + (right - left) / 2;
+                    break;
+                case 1:
+                    left = cursor+1;
+                    cursor = left + (right - left) / 2;
+                    break;
+                case 0:
+                    return cursor;
+            }
+        }
+        return 0;
+    }
+};
