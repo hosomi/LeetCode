4f484e237dad2f449f13edaf0324e9aa7473f7f0
hosomi <hosomi@users.noreply.github.com>
2021-12-02 09:22:06 +0900

    Create #0398.random-pick-index.cpp

---
+++ #0398.random-pick-index.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    Solution(vector<int>& nums) {
+
+        this->nums = &nums;
+    }
+
+    int pick(int target) {
+
+        int count = 1;
+        int result = 0;
+        for (int i = 0; i < nums->size(); ++i) {
+            if ((*nums)[i] == target) {
+                if (rand() % count == 0) {
+                    result = i;
+                }
+                ++count;
+            }
+        }
+        return result;
+    }
+
+private:
+    vector<int>* nums;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
