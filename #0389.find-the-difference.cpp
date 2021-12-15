7a9cd0345c4985585b05d5f0ccfa5377275f46ea
hosomi <hosomi@users.noreply.github.com>
2021-12-15 09:14:06 +0900

    Create #0389.find-the-difference.cpp

---
+++ #0389.find-the-difference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+
+        vector<int> counter(26, 0);
+        for(char c : t){
+            ++counter[c - 'a'];
+        }
+        for(char c : s){
+            --counter[c - 'a'];
+        }
+        return 'a' + std::find(counter.begin(), counter.end(), 1) - counter.begin();
+    }
+};
