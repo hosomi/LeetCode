165d10a4cea45c834c9e73931c8f15883e5ada5d
hosomi <hosomi@users.noreply.github.com>
2021-12-17 19:11:34 +0900

    Create #0385.mini-parser.cpp

---
+++ #0385.mini-parser.cpp
@@ -0,0 +1,77 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    NestedInteger deserialize(string s) {
+
+        int size = s.size();
+        NestedInteger result;
+        if (size == 0) {
+            return result;
+        }
+
+        if (s[0] != '[') {
+            result.setInteger(stoi(s));
+            return result;
+        }
+
+        stack<NestedInteger> st;
+        string tmp = "";
+        for (int i = 0; i <= size - 1; i++) {
+            if (s[i] == ' ') {
+                continue;
+            }
+
+            if (s[i] != '[' and s[i] != ']' and s[i] != ',') {
+                tmp += s[i];
+            }
+            if (s[i] == ',' or s[i] == ']') {
+                if (tmp != "") {
+                    st.top().add(stoi(tmp));
+                }
+                tmp = "";
+            }
+            if (s[i] == '[') {
+                NestedInteger tmp_ni;
+                st.push(tmp_ni);
+            }
+            if (s[i] == ']') {
+                if (st.size() == 1) {
+                    continue;
+                }
+
+                auto a = st.top();
+                st.pop();
+                st.top().add(a);
+            }
+        }
+        return st.top();
+    }
+};
