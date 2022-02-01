270f35053ac31d34ccc3cdccb17b443c8503a816
hosomi <hosomi@users.noreply.github.com>
2022-02-01 09:11:53 +0900

    Create #1328.break-a-palindrome.cpp

---
+++ #1328.break-a-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        
+        int size = palindrome.size();
+        if (size == 1) {
+            return "";
+        }
+        
+        for (int i = 0; i < size / 2; ++i) {
+            if (palindrome[i] != 'a') {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        palindrome.back() = 'b';
+        return palindrome;
+    }
+};
