5b52d72779fedbcf1ed710c99f6a1725584d9c7c
hosomi <hosomi@users.noreply.github.com>
2022-06-08 09:05:14 +0900

    Create #1332.remove-palindromic-subsequences.cpp

---
+++ #1332.remove-palindromic-subsequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+
+        if (s.empty()) {
+            return 0;
+        }
+
+        if (string(s.rbegin(), s.rend()) == s) {
+            return 1;
+        }
+
+        return 2;
+    }
+};
