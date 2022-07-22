158b9421e9d1149d118f77ab3a693579ffa0c1e4
hosomi <hosomi@users.noreply.github.com>
2022-07-23 08:21:42 +0900

    Create #1037.valid-boomerang.cpp

---
+++ #1037.valid-boomerang.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>>& points) {
+
+        if ((points[0][0] == points[1][0] 
+            && points[0][1] == points[1][1]) || (points[0][0] == points[2][0]
+            && points[0][1] == points[2][1]) || (points[1][0] == points[2][0]
+            && points[1][1] == points[2][1])) {
+            return false;
+        }
+        
+        if (points[0][0] == points[1][0] || points[0][0] == points[2][0]) {
+            if ((points[0][0] == points[1][0]) && (points[0][0] == points[2][0])) {
+                return false;
+            }
+            return true;
+        }
+        
+        if (points[0][0] == points[2][0]) {
+            return false;
+        }
+
+        if ((points[0][1]-points[2][1]) /
+            (double)(points[0][0] - points[2][0]) == (points[0][1] - points[1][1]) /
+            (double)(points[0][0] - points[1][0])) {
+            return false;
+        }
+
+        return true;
+    }
+};
