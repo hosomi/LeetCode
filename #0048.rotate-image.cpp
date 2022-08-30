30385f47e22ce3373a842d1cf56f40c226eb27e9
hosomi <hosomi@users.noreply.github.com>
2022-08-30 09:06:28 +0900

    Update #0048.rotate-image.cpp

--- #0048.rotate-image.cpp
+++ #0048.rotate-image.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
 
-        int n = matrix.size();
-        for (int i = 0; i < matrix.size(); i++){
-            for (int j = i; j < matrix.size(); j++){
-                swap(matrix[i][j], matrix[j][i]);
+        int size = matrix.size();
+        for (int i = 0; i < size; i++) {
+            for (int j = i; j < size; j++) {
+                std::swap(matrix[i][j], matrix[j][i]);
             }
         }
 
-        for (int i = 0; i < matrix.size(); i++){
-            for (int j = 0; j < matrix.size() / 2; j++){
-                swap(matrix[i][j], matrix[i][n-j-1]);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size / 2; j++) {
+                std::swap(matrix[i][j], matrix[i][size - j - 1]);
             }
         }
     }
