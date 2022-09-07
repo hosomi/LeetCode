602f7507a6b96b60fbf89b57fd8c5ef8807ebd50
hosomi <hosomi@users.noreply.github.com>
2022-09-07 09:09:59 +0900

    Create #0606.construct-string-from-binary-tree.cpp

---
+++ #0606.construct-string-from-binary-tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+
+        string result = std::to_string(root->val);
+        if (root->left && root->right) {
+            result += "(" + this->tree2str(root->left) + ")";
+            result += "(" + this->tree2str(root->right) + ")";
+        } else if (root->left && !root->right) {
+            result += "(" + this->tree2str(root->left) + ")";
+        } else if (!root->left && root->right) {
+            result += "()";
+            result += "(" + this->tree2str(root->right) + ")";
+        }
+        return result;
+    }
+};
