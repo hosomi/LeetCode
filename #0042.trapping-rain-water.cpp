0b8b1226433b086851a9bd4f9a8d684b222bd268
hosomi <hosomi@users.noreply.github.com>
2022-09-18 11:46:59 +0900

    Update #0042.trapping-rain-water.cpp

--- #0042.trapping-rain-water.cpp
+++ #0042.trapping-rain-water.cpp
@@ -3,18 +3,26 @@ public:
     int trap(vector<int>& height) {
 
         int size = height.size();
-        auto itHead = cbegin(height);
-        int h;
-        auto itNext = cend(height);
-        int n;
-
-        int result = 0;
-        for (int i = 0; i < size; ++i) {
-            h = *max_element(itHead, itHead + i + 1);
-            n = *max_element(itHead + i, itNext);
-            result += min(h, n) - height[i];
+        if (size == 0) {
+            return 0;
         }
 
+        int i;
+        int left[size];
+        int right[size];
+        left[0] = height[0];
+        for(i = 1; i < size; i++) {
+            left[i] = std::max(left[i - 1], height[i]);
+        }
+        
+        for(i = size - 2; i >= 0; i--) {
+            right[i] = std::max(right[i + 1], height[i]);
+        }
+
+        int result = 0;
+        for (i = 1; i <= size-2; i++) {
+            result += (std::min(left[i], right[i]) - height[i]);
+        }
         return result;
     }
 };
