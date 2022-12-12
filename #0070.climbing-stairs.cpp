25bc9f60ffc6275ae8dbe8a9faa2f72964237096
hosomi <hosomi@users.noreply.github.com>
2022-12-12 09:09:31 +0900

    Update #0070.climbing-stairs.cpp

--- #0070.climbing-stairs.cpp
+++ #0070.climbing-stairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-
-        int step = 1;
+    
+        int result = 1;
         int last = 1;
         for (int i = 2; i <= n; i++) {
-            last += step;
-            swap(step, last);
+            last += result;
+            swap(result, last);
         }
-
-        return step;
+        return result;
     }
 };
