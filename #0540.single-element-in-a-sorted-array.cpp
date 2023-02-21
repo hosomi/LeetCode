58fbcd405e68e12cc4bda57b650526ccb43d41da
hosomi <hosomi@users.noreply.github.com>
2023-02-21 09:01:58 +0900

    Create #0540.single-element-in-a-sorted-array.cpp

---
+++ #0540.single-element-in-a-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
