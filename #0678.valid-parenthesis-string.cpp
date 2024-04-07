4695161653f9269212529bd01d09d14e5293f7c7
hosomi <hosomi@users.noreply.github.com>
2024-04-07 09:09:58 +0900

    Create #0678.valid-parenthesis-string.cpp

---
+++ #0678.valid-parenthesis-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+
+        int low = 0;  
+        int high = 0;
+        for (const char c : s) {
+            switch (c) {
+                case '(':
+                    ++low;
+                    ++high;
+                    break;
+                case ')':
+                    low = max(0, --low);
+                    --high;
+                    break;
+                case '*':
+                    low = max(0, --low);
+                    ++high;
+                    break;
+            }
+            
+            if (high < 0) {
+                return false;
+            }
+        }
+        return low == 0;
+    }
+};
