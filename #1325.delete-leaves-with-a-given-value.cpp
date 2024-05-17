5e45d2e8b35a6e792e912e7fe5c1dafd3ed2a510
hosomi <hosomi@users.noreply.github.com>
2024-05-17 09:03:32 +0900

    Create #1325.delete-leaves-with-a-given-value.cpp

---
+++ #1325.delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+
+        if (root == NULL) {
+            return NULL;
+        }
+
+        TreeNode* left = removeLeafNodes(root->left, target);
+        TreeNode* right = removeLeafNodes(root->right, target);
+        if (left == NULL && right == NULL && root->val == target) {
+            return NULL;
+        }
+        
+        root->left = left;
+        root->right = right;
+        return root;
+    }
+};
