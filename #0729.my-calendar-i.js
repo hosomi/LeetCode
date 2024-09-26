a87211bea42ad56b9d8de23c14d5243a58ee8304
hosomi <hosomi@users.noreply.github.com>
2024-09-26 09:05:02 +0900

    Create #0729.my-calendar-i.js

---
+++ #0729.my-calendar-i.js
@@ -0,0 +1,25 @@
+
+var MyCalendar = function() {
+    this.calendar = [];
+};
+
+/** 
+ * @param {number} start 
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function(start, end) {
+    for (let i of this.calendar) {
+        if (i[0] < end && i[1] > start) {
+            return false;
+        }
+    }
+    this.calendar.push([start, end]);
+    return true;
+};
+
+/** 
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
