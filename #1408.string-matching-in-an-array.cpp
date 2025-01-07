cd9ac184c0f001b252ae488e1170edaec1563dc3
hosomi <hosomi@users.noreply.github.com>
2025-01-07 09:08:58 +0900

    Create #1408.string-matching-in-an-array.cpp

---
+++ #1408.string-matching-in-an-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+
+        vector<string> res;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (words[i] != words[j]
+                    && words[j].find(words[i]) != -1) {
+                    res.push_back(words[i]);
+                    break;                    
+                }
+            }
+        }
+        return res;
+    }
+};
