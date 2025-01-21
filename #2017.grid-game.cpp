aaaa5b5cd3fe85982a382c03d6e93b7e95455820
hosomi <hosomi@users.noreply.github.com>
2025-01-21 09:09:12 +0900

    Create #2017.grid-game.cpp

---
+++ #2017.grid-game.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+
+        long long topSum = accumulate(begin(grid[0]), end(grid[0]), 0LL);
+        long long  bottomSum = 0;
+        long long ans = LLONG_MAX;
+        for (int i = 0; i < grid[0].size(); ++i) {
+            topSum -= grid[0][i];
+            ans = std::min(ans, std::max(topSum, bottomSum));
+            bottomSum += grid[1][i];
+        }
+        return ans;
+    }
+};
