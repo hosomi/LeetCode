59b904d393351a95b0be032bf85e4aa61624652c
hosomi <hosomi@users.noreply.github.com>
2025-02-05 09:08:08 +0900

    Create #1790.check-if-one-string-swap-can-make-strings-equal.cpp

---
+++ #1790.check-if-one-string-swap-can-make-strings-equal.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+
+        if (s1 == s2) {
+            return true;
+        }
+
+        int left = 0;
+        int right = s1.size() - 1;
+        while (s1[left] == s2[left]) {
+            left++;
+        }
+        while (s1[right] == s2[right]) {
+            right--;
+        }
+        std::swap(s2[right], s2[left]);
+        return s1 == s2;
+    }
+};
