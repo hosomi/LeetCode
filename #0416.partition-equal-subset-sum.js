203b4079f7a5e9b9083674cb108b611319023feb
hosomi <hosomi@users.noreply.github.com>
2025-04-07 09:07:34 +0900

    Create #0416.partition-equal-subset-sum.js

---
+++ #0416.partition-equal-subset-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+
+    const total = nums.reduce((a, b) => a + b, 0);
+    if (total % 2) {
+        return false;
+    }
+
+    const target = total / 2;
+    let everySum = new Set([0]);
+    for (let i = nums.length - 1; i >= 0; i--) {
+        const nextSums = new Set()
+        for (const sum of everySum) {
+			if (nums[i] + sum == target) {
+                return true;
+            }
+            nextSums.add(sum);
+            nextSums.add(sum + nums[i]);
+        }
+        everySum = nextSums;
+    }
+    
+    return everySum.has(target);
+};
