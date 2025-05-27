74f8229199f784bb35ab11f681c25e821fc180ce
hosomi <hosomi@users.noreply.github.com>
2025-05-27 09:11:52 +0900

    Create #2894.divisible-and-non-divisible-sums-difference.cpp

---
+++ #2894.divisible-and-non-divisible-sums-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        
+        int num1 = 0;
+        int num2 = 0;
+        for (int i = 0; i < n +1; i++) {   
+            if (i % m == 0) {
+                num2 = num2 + i;
+            } else {
+                num1 = num1 + i;
+            }
+        }
+        return num1 - num2;
+    }
+};
