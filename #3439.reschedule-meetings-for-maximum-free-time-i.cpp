0b621748fdc04bb5e04536b92105d701a855be56
hosomi <hosomi@users.noreply.github.com>
2025-07-09 09:11:53 +0900

    Create #3439.reschedule-meetings-for-maximum-free-time-i.cpp

---
+++ #3439.reschedule-meetings-for-maximum-free-time-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+
+        vector<int> gap(1, startTime[0]);
+        for (int i = 1; i < startTime.size(); ++i) {
+            gap.push_back(startTime[i] - endTime[i-1]);
+        }
+
+        gap.push_back(eventTime - endTime.back());
+        
+        int ans = 0, sum = 0;
+        for (int i = 0; i < gap.size(); ++i) {
+            sum += gap[i] - ((i >=  k+1)? gap[i - (k+1)] : 0);
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
