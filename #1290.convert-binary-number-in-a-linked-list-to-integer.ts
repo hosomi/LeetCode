b873ab1ff1c41ed92ef320de7e29e48c22670dfe
hosomi <hosomi@users.noreply.github.com>
2025-07-14 09:10:22 +0900

    Create #1290.convert-binary-number-in-a-linked-list-to-integer.ts

---
+++ #1290.convert-binary-number-in-a-linked-list-to-integer.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+    let binary = "";
+    let temp = head;
+    while(temp){
+        binary += temp.val;
+        temp = temp.next;
+    }
+    return parseInt(binary, 2);
+};
