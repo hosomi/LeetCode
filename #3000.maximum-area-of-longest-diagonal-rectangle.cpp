b61ecf91a903b6cb8f15758d4c4d01c9f3066b9a
hosomi <hosomi@users.noreply.github.com>
2025-08-26 09:05:38 +0900

    Create #3000.maximum-area-of-longest-diagonal-rectangle.cpp

---
+++ #3000.maximum-area-of-longest-diagonal-rectangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+
+        int area = 0;
+        int diag = 0;
+        for (auto d : dimensions) {
+            int t = d[0] * d[0] + d[1] * d[1];
+            if (t > diag || (t == diag && d[0]*d[1] > area)) {
+                diag = t;
+                area = d[0]*d[1];
+            }
+        }
+        return area;
+    }
+};
